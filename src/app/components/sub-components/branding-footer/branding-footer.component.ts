944d7b8727c6c21e0d8b9ad402c48c2ead423707
dominikazb <domi.biesiada@gmail.com>
2021-07-07 21:03:26 +0200

    Task 0 - Adding branding logos to results page

--- src/app/components/sub-components/branding-footer/branding-footer.component.ts
+++ src/app/components/sub-components/branding-footer/branding-footer.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'branding-footer',
   templateUrl: './branding-footer.component.html'
 })
-export class BrandingFooterComponent { }
+export class BrandingFooterComponent {
+  @Input() showBrandingFooterText: boolean;
+}
